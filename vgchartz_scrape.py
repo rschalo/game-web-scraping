944daee36d64061920419f01297dfff334f347a2
Reed Schalo <reed0schalo@gmail.com>
2019-08-28 10:57:29 -0700

    Added for loop to write game names to file

--- vgchartz_scrape.py
+++ vgchartz_scrape.py
@@ -14,6 +14,8 @@ ns_html_file.close()
 soup_file_prep = open('nintendo_switch_game_rankings')
 ns_file_soup = bs4.BeautifulSoup(soup_file_prep, features='lxml')
 #convert downloaded HTML to bs object
+ns_game_titles = open('ns_game_names', 'w')
 for i in ns_file_soup.select('a[href^="http://www.vgchartz.com/game/"]'):
-    print(i.text)
-#iterate through and print the video game name, credit to Anand S Kumar at https://stackoverflow.com/questions/32126358/how-do-i-get-the-value-of-a-soup-select
+    ns_game_titles.write(i.text)
+ns_game_titles.close()
+#write game names to a file
