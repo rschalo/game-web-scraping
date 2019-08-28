d4745ece81d6b11b9dbbe6a7c10df2da1f12a738
rschalo <reed0schalo@gmail.com>
2019-08-27 23:02:27 -0700

    Saving downloaded URL to local drive

--- vgchartz_scrape.py
+++ vgchartz_scrape.py
@@ -7,3 +7,8 @@ import bs4
 res = requests.get('http://www.vgchartz.com/gamedb/games.php?console=NS')
 res.raise_for_status()
 #determine if the requests object is created successfully
+ns_html_file = open('nintendo_switch_game_rankings', 'wb')
+for chunk in res.iter_content(100000):
+    ns_html_file.write(chunk)
+ns_html_file.close()
+#create a local copy of HTML ranking page at URL in line 7
